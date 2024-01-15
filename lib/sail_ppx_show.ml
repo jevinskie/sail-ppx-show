24d69899883c8f88c529e65bc4060f4f4bcf41c0
Jevin Sweval <jevinsweval@gmail.com>
2024-01-15 00:56:54 -0500

    dunno

--- lib/sail_ppx_show.ml
+++ lib/sail_ppx_show.ml
@@ -1,20 +1,127 @@
+(* open Ppxlib *)
+
+(* let expand ~ctxt env_var =
+     let loc = Expansion_context.Extension.extension_point_loc ctxt in
+     match Sys.getenv env_var with
+     | value -> Ast_builder.Default.estring ~loc value
+     | exception Not_found ->
+         let ext =
+           Location.error_extensionf ~loc "The environement variable %s is unbound"
+             env_var
+         in
+         Ast_builder.Default.pexp_extension ~loc ext
+
+   let my_extension =
+     Extension.V3.declare "get_env" Extension.Context.expression
+       Ast_pattern.(single_expr_payload (estring __))
+       expand
+
+   let rule = Ppxlib.Context_free.Rule.extension my_extension
+   let () = Driver.register_transformation ~rules:[ rule ] "get_env" *)
+
+(*
+let transform a b c =
+  let module Metaopt (Item : Ppxlib.payload) = struct
+    let map (super : Item.t Ast.map) (item : Item.t) : Item.t =
+      Ppxlib.Ast_helper.with_default_loc (Item.to_loc item) @@ fun () ->
+      match Option.bind (Item.destruct_extension item) Options.handle with
+      | None -> super item
+  end in
+  let map = object end in
+  b a [c;] *)
+
+(* let map = object (self)
+     inherit Ppxlib.Ast_traverse.map
+
+     method! structure s = self#structure s
+     method! signature s = self#signature s
+
+     (* method! structure s =
+       transform self#structure Ocaml_common.Ast_mapper.default_mapper s *)
+     (* method! structure _ = Ocaml_common.Ast_mapper.default_mapper.structure *)
+
+     (* method! signature s =
+       transform self#structure Ocaml_common.Ast_mapper.default_mapper s *)
+   end *)
+
+(* let () =
+   Ppxlib.Driver.register_transformation "get_env"
+     ~preprocess_impl:map#structure
+     ~preprocess_intf:map#signature *)
+
+(* open Ppxlib.Asttypes
+   open Ppxlib.Parsetree
+   open Ocaml_common.Ast_mapper
+
+   let test_mapper argv =
+     { default_mapper with
+       expr = fun mapper expr ->
+         match expr with
+         | { pexp_desc = Pexp_extension ({ txt = "test"; _ }, PStr []); _} ->
+           Ast_helper.Exp.constant Pconst_integer "42" 'z'
+         | other -> default_mapper.expr mapper other; }
+
+   let () =
+     register "get_env" test_mapper *)
+
+(* open Ppxlib
+   class mapper =
+     object (self)
+       inherit  Ast_traverse.map
+       method! signature signature =
+         (signature |> (List.map (Ppxlib.Ast_helperSignature.mapSignatureItem self))) |>
+           List.concat
+       method! structure structure =
+         (structure |> (List.map (Structure.mapStructureItem self))) |>
+           List.concat
+     end
+   let structure_mapper s = (new mapper)#structure s
+   let signature_mapper s = (new mapper)#signature s
+   ;;Ppxlib.Driver.register_transformation ~preprocess_impl:structure_mapper
+       ~preprocess_intf:signature_mapper "decco" *)
+
+open Stdppx
 open Ppxlib
 
-let expand ~ctxt env_var =
-  let loc = Expansion_context.Extension.extension_point_loc ctxt in
-  match Sys.getenv env_var with
-  | value -> Ast_builder.Default.estring ~loc value
-  | exception Not_found ->
-      let ext =
-        Location.error_extensionf ~loc "The environement variable %s is unbound"
-          env_var
-      in
-      Ast_builder.Default.pexp_extension ~loc ext
+let pprint_ctxt ctxt =
+  let tool_name = Expansion_context.Base.tool_name ctxt in
+  let input_name = Expansion_context.Base.input_name ctxt in
+  let file_path =
+    Code_path.file_path @@ Expansion_context.Base.code_path @@ ctxt
+  in
+  Printf.printf "tool_name: %s\ninput_name: %s\nfile_path: %s\n" tool_name
+    input_name file_path
 
-let my_extension =
-  Extension.V3.declare "get_env" Extension.Context.expression
-    Ast_pattern.(single_expr_payload (estring __))
-    expand
+let side_print_ctxt =
+  object
+    inherit Ast_traverse.map_with_expansion_context_and_errors as super
 
-let rule = Ppxlib.Context_free.Rule.extension my_extension
-let () = Driver.register_transformation ~rules:[ rule ] "get_env"
+    method! structure ctxt st =
+      pprint_ctxt ctxt;
+      super#structure ctxt st
+
+    method! signature ctxt sg =
+      pprint_ctxt ctxt;
+      super#signature ctxt sg
+  end
+
+let () =
+  Driver.V2.(
+    register_transformation
+      ~impl:(fun ctxt structure ->
+        let structure, errors = side_print_ctxt#structure ctxt structure in
+        List.map errors ~f:(fun error ->
+            Ast_builder.Default.pstr_extension
+              ~loc:(Location.Error.get_location error)
+              (Location.Error.to_extension error)
+              [])
+        @ structure)
+      ~intf:(fun ctxt signature ->
+        let signature, errors = side_print_ctxt#signature ctxt signature in
+        List.map errors ~f:(fun error ->
+            Ast_builder.Default.psig_extension
+              ~loc:(Location.Error.get_location error)
+              (Location.Error.to_extension error)
+              [])
+        @ signature)
+      "print_ctxt")
